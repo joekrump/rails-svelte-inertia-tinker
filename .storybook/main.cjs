52e45573b80292dbb09d55f7bad107f78f3deba4
Joe Krump <krump.joseph@gmail.com>
2023-10-17 21:18:04 -0400

    Add storybook and setup
    Also updated yarn to version 3 to avoid an issue where both storybook and cypress depend on the same node module (string-width) but different versions. See: https://github.com/cypress-io/cypress/issues/27370\#issuecomment-1649715561

---
+++ .storybook/main.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/svelte",
+  "core": {
+    "builder": "@storybook/builder-vite",
+    "disableTelemetry": true
+  },
+  "svelteOptions": {
+    "preprocess": require("../svelte.config.cjs").preprocess
+  },
+  "features": {
+    "storyStoreV7": true
+  }
+}
